a3556839eb736441001162004e802d268d9b01ba
middtown <aarontm9@gmail.com>
2017-12-15 12:24:57 -0700

    index page change to landing page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 console.log("java up and running");
 
 //list of questions and list of answers and variables to maintain
-var questionBank = ["Who has a big head?", "Who has a blanket?", "Who plays the piano?", "Who has a best bird friend?"];
-var solutionBank = ["Charlie Brown", "Linus", "Schroeder", "Snoopy"];
+var questionBank = ["Who has a big head?", "Who has a blanket?", "Who plays the piano?", "Who has a best bird friend?", "Who is Charlie Brown's sister?", "Who is the dirtiest character?",
+					"Who is known to be selfish, crabby or just bossy?", "His arch nemisis is a kite?", "She is the sweetest and the smartes of them all.","Who does Schroeder have sitting on his piano?",
+					"Who hosted a Halloween party at her house in It’s the Great Pumpkin Charlie Brown?", "Who portrayed a shepherd in A Charlie Brown Christmas?", "Which character has always believes in the 'Great Pumpkin'?",
+					"He sometimes refers to the cloud that surrounds him with pride as the dust of ancient civilizations."];
+var solutionBank = ["Charlie Brown", "Linus", "Schroeder", "Snoopy", "Sally Brown", "Pig Pen", "Lucy", "Charlie Brown", "Marcie", "Lucy", "Sally Brown", "Linus", "Linus", "Pig Pen"];
 var trivia = document.getElementById("questionBoard");
 var playerOneScore = 0;
 var playerTwoScore = 0;
@@ -24,7 +27,6 @@ function getCurrentPlayer() {
 	console.log(currentPlayerScore);
 //generate random question
 function generateQuestion() {
-
 	question = Math.floor(Math.random() * questionBank.length);
 	counter += 1;
 	answer = solutionBank[question];
@@ -32,13 +34,14 @@ function generateQuestion() {
 		//find player's turn
 		if (counter === 1) {
 		alert("player one goes first!");
-	} else if (counter === 7) {
+	} else if (counter === 8) {
 		alert("player two's turn");
 		generateQuestion();
 		//player two turn;
-	} else if (counter === 14) {
+	} else if (counter === 15) {
 		//player two stuff goes here?? -- help --
 		checkWinner();
+		play3();
 		alert("game over");
 		newGame();
 	} 
@@ -53,7 +56,9 @@ function generateQuestion() {
 function checkAnswer(e) {
 	//console.log(playerOneScore);
     if ( e.textContent == answer){
+    	play2();
 	    getCurrentPlayer();
+	    console.log("right answer");
 
 		var newDiv = document.createElement("div"); 
 		//and give it some content 
@@ -71,7 +76,9 @@ function checkAnswer(e) {
 	  		playerTwoScore++;
 	  		playerTwo.innerHTML =  playerTwoScore;
 	  	}
+
     } else {
+    	play1();
 	    var otherDiv = document.createElement("div"); 
 		//and give it some content 
 	  	var otherContent = document.createTextNode("You Block Head"); 
@@ -87,7 +94,6 @@ function checkAnswer(e) {
 	    }
     generateQuestion();
 }
-
 function checkWinner(){
 	if (playerOneScore === playerTwoScore) {
 		var newDiv = document.createElement("div"); 
@@ -154,10 +160,19 @@ var woodstock = document.getElementById("woodstock").addEventListener("click", f
 });
 
 
-function play(){
-       var audio = document.getElementById("audio");
-       audio.play();
+function play1() {
+       var audio1 = document.getElementById("audio1");
+       audio1.play();
 }
+function play2() {
+       var audio2 = document.getElementById("audio2");
+       audio2.play();
+}
+function play3() {
+       var audio3 = document.getElementById("audio3");
+       audio3.play();
+}
+
 
 //-------------- GRAVE YARD --------------//
 
